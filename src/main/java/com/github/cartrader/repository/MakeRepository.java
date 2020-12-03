f216d143ba307c96f01e3429ab1d50591c42e613
Anthony <maniatis.anthony@gmail.com>
2020-12-03 17:57:41 +0200

    Added the @Repository stereotype that was missing from some Repositories

--- src/main/java/com/github/cartrader/repository/MakeRepository.java
+++ src/main/java/com/github/cartrader/repository/MakeRepository.java
@@ -1,9 +1,11 @@
 package com.github.cartrader.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.github.cartrader.entity.Make;
 
+@Repository
 public interface MakeRepository extends JpaRepository<Make, Integer> {
 
 }
--- src/main/java/com/github/cartrader/repository/ModelRepository.java
+++ src/main/java/com/github/cartrader/repository/ModelRepository.java
@@ -1,9 +1,11 @@
 package com.github.cartrader.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.github.cartrader.entity.Model;
 
+@Repository
 public interface ModelRepository extends JpaRepository<Model, Integer> {
 
 }
